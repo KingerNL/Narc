53d96b65ff27218f4c531f5775195d7cda4a75b5
Mart <mart.veldkamp@gmail.com>
2023-07-12 18:34:35 +0200

    Added a basic template.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -5,6 +6,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Windowing; // Needed for AppWindow .
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,25 +14,55 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinRT.Interop;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Project_Narc
-{
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
-    {
+namespace Project_Narc {
+
+    public sealed partial class MainWindow : Window {
+        private AppWindow m_AppWindow;
+
         public MainWindow()
         {
             this.InitializeComponent();
+
+            m_AppWindow = GetAppWindowForCurrentWindow();
+            m_AppWindow.Title = "";
+
+            var titlebar = m_AppWindow.TitleBar;
+            titlebar.IconShowOptions = IconShowOptions.HideIconAndSystemMenu;
+            titlebar.ButtonBackgroundColor = Colors.Transparent;
+            titlebar.ButtonHoverBackgroundColor = Colors.Transparent;
+            titlebar.ButtonPressedBackgroundColor = Colors.Transparent;
+            titlebar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                var titleBar = m_AppWindow.TitleBar;
+                // Hide default title bar.
+                titleBar.ExtendsContentIntoTitleBar = true;
+            }
+            else
+            {
+                // In the case that title bar customization is not supported, hide the custom title bar
+                // element.
+                AppTitleBar.Visibility = Visibility.Collapsed;
+            }
+
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        //private void myButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    myButton.Content = "Clicked";
+        //}
+        private AppWindow GetAppWindowForCurrentWindow()
         {
-            myButton.Content = "Clicked";
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# Narc - The Knock-off Arc (For windows)
+
+Hi there, because it's summer vacation. I decided to work on some proejcts because I was bored. One of these projects is a simple Web-browser inspired by [The Browser Company](https://thebrowser.company/).
+
+___
+
+It's build in C#, and I know they are making it in Swift. So don't expect anything to fancy from my part... It was just ment as a fun side project.
